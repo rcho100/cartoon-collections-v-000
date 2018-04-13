455054911496d0ab340aa3bdc105ed20ebd8d9ea
Richard Cho <richardcho100@gmail.com>
2018-04-13 15:12:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|name, index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  
 end
 
 def long_planeteer_calls# code an argument here
