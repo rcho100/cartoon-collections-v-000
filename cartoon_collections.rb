7c260aa7674769c3dde27fb215e26d0014f3e319
Richard Cho <richardcho100@gmail.com>
2018-04-13 15:31:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ def long_planeteer_calls(calls_long)
   calls_long.any? { |words| words.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.detect do |food|
+    food == "cheddar" || food == "gouda" || food == "camembert"
+  end
 end
